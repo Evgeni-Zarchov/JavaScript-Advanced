b3860c7d1363636e0431034ccdf700d5c3f7c01a
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2024-04-28 11:21:55 +0300

    Update 01-Fruits.js

--- Syntax-Functions-And-Statements-EXE/01-Fruits.js
+++ Syntax-Functions-And-Statements-EXE/01-Fruits.js
@@ -3,11 +3,13 @@ function fruits(fruits, weightInGram, pricePerFruit) {
 
     const totalPrice = fruitsInKilo * Number(pricePerFruit);
 
-    const output = `I need $${totalPrice.toFixed(2)} to buy ${fruitsInKilo.toFixed(2)} kilograms ${fruits}.`;
+    const output = `I need $${totalPrice.toFixed(
+        2
+    )} to buy ${fruitsInKilo.toFixed(2)} kilograms ${fruits}.`;
 
     console.log(output);
 }
 
-fruits('orange', 2500, 1.80);
+fruits("orange", 2500, 1.8);
 
-fruits('apple', 1563, 2.35 );
+fruits("apple", 1563, 2.35);
