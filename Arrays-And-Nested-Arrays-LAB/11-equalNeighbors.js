745c71386c4b0344bd0b5bfc5040880c5b7f8937
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2024-05-02 11:24:32 +0300

    Update 11-equalNeighbors.js

--- Arrays-And-Nested-Arrays-LAB/11-equalNeighbors.js
+++ Arrays-And-Nested-Arrays-LAB/11-equalNeighbors.js
@@ -1,42 +1,41 @@
 function equalNeighborsInMatrix(arr) {
     let counter = 0;
 
-    for (let rows = 0; rows < arr.length; rows++) {
-        let innerArray = arr[rows];
+    for (let cols = 0; cols < arr.length; cols++) {
+        let innerArray = arr[cols];
 
-        for (let cols = 0; cols < innerArray.length; cols++) {
-            if (rows - 1 < 0 || cols - 1 < 0) {
-                continue;
-            }
+        for (let rows = 0; rows < innerArray.length; rows++) {
+            
 
-            if (arr[rows][cols] == arr[rows - 1][cols]) {
+            if (cols - 1 >= 0 && arr[cols][rows] == arr[cols - 1][rows]) {
                 counter++;
             }
 
-            if (arr[rows][cols] == arr[rows][cols - 1]) {
+            if (rows - 1 >=0 && arr[cols][rows] == arr[cols][rows - 1]) {
                 counter++;
             }
         }
     }
 
-    console.log(counter);;
+    return counter;
 }
 
-equalNeighborsInMatrix([
+console.log(equalNeighborsInMatrix([
     [2,2,5,7,4],
     [4,0,5,3,4],
-    [2,5,5,4,2]]);
+    [2,5,5,4,2]]));
 
 
-// equalNeighborsInMatrix([
-//     ['2', '3', '4', '7', '0'],
-//     ['4', '0', '5', '3', '4'],
-//     ['2', '3', '5', '4', '2'],
-//     ['9', '8', '7', '5', '4']]
-// );
+console.log(equalNeighborsInMatrix([
+    ['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4']]
+));
 
-// equalNeighborsInMatrix([
-//     ['test', 'yes', 'yo', 'ho'],
-//     ['well', 'done', 'yo', '6'],
-//     ['not', 'done', 'yet', '5']]
-// );
+console.log(equalNeighborsInMatrix([
+    ['test', 'yes', 'yo', 'ho'],
+    ['well', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', '5']]
+));
+// epxect 2 
